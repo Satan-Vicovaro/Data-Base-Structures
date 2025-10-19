116a130984e870d0926c89f6fd8a0daad93239e7
Łukasz Kołakowski <kolak.lukass1@gmail.com>
2025-10-19 18:21:19 +0200

    Config update

--- merge_sort/Config.hpp
+++ merge_sort/Config.hpp
@@ -3,5 +3,8 @@
 namespace config {
 const int print_file_stream_size = 100;
 const int record_char_size = 10;
-const int in_memory_chunk_element_count = 10;
+const int in_memory_chunk_element_count = 5;
+
+const int records_to_load = 3;
+const int max_buffer_count = 3;
 } // namespace config
