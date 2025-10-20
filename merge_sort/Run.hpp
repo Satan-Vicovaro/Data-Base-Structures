3c4e89b1c05e374a2df086fea8d1e7f39c0c1d45
Łukasz <kolak.lukass1@gmail.com>
2025-10-20 12:41:37 +0200

    fixed bug in reading data from file

--- merge_sort/Run.hpp
+++ merge_sort/Run.hpp
@@ -34,10 +34,17 @@ struct Run {
   get_next_records(int record_num, std::fstream &file_stream) {
     bool end_of_run = false;
     int records_read = 0;
-    std::vector<Record> result = {};
-    std::string line;
+    std::vector<Record> result;
+    // result.emplace_back(current_record_);
+    // record_num++;
 
+    std::string line;
+    file_stream.clear();
     file_stream.seekg(current_record_pos_, std::ios::beg);
+    if (!file_stream) {
+      std::cerr << "seekg failed at position " << current_record_pos_ << '\n';
+      return {result, true}; // Early return to avoid undefined behavior
+    }
 
     while (std::getline(file_stream, line) && records_read < record_num) {
       Record next_record = Record(line);
@@ -52,7 +59,7 @@ struct Run {
 
     if (file_stream.eof()) {
       end_of_run = true;
-      return std::make_tuple(result, end_of_run);
+      return {result, end_of_run};
     }
 
     std::getline(file_stream, line);
@@ -60,20 +67,12 @@ struct Run {
     Record next_record = Record(line);
     if (next_record < current_record_) {
       end_of_run = true;
-      return std::make_tuple(result, end_of_run);
+      return {result, end_of_run};
     }
 
     current_record_ = next_record;
     current_record_pos_ = file_stream.tellg();
 
-    return std::make_tuple(result, end_of_run);
-  }
-
-  std::vector<Run> generate_runs(int runs_num, std::fstream &file_stream) {
-    std::streampos streampos = 0;
-    std::vector<Run> runs = {};
-    runs.emplace_back(Run(streampos, file_stream));
-    while (runs.size() < runs_num) {
-    }
+    return {result, end_of_run};
   }
 };
