f83e7ffbd2456cf2ea6a9d0f6e4096590fb78265
Łukasz Kołakowski <kolak.lukass1@gmail.com>
2025-11-04 00:12:42 +0100

    Started intorducing IOManager

--- merge_sort/Run.hpp
+++ merge_sort/Run.hpp
@@ -1,8 +1,11 @@
 #pragma once
+#include "IOManager.hpp"
 #include "Record.hpp"
 #include <fstream>
 #include <iosfwd>
 #include <iostream>
+#include <iterator>
+#include <memory>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -10,72 +13,53 @@
 struct Run {
   std::streampos current_record_pos_;
   Record current_record_;
+  std::shared_ptr<IOManager> io_manager_;
 
   Run() {
     current_record_pos_ = 0;
     current_record_ = Record();
+    io_manager_ = nullptr;
   }
 
-  Run(std::streampos initial_record_pos_, std::fstream &file_stream) {
-
-    file_stream.seekg(initial_record_pos_, std::ios::beg);
-    std::string line = std::string();
-    if (std::getline(file_stream, line)) {
-      current_record_ = Record(line);
-    } else {
-      std::cout << "Could not initialize the run properrly";
-      current_record_ = Record();
-    }
-    current_record_pos_ = file_stream.tellg();
+  Run(std::streampos initial_record_pos_, std::fstream &file_stream,
+      std::shared_ptr<IOManager> manager) {
+    io_manager_ = manager;
+    current_record_pos_ = initial_record_pos_;
+    current_record_ = Record();
   }
 
   std::tuple<std::vector<Record>, bool>
   get_next_records(int record_num, std::fstream &file_stream) {
     bool end_of_run = false;
-    int records_read = 0;
-    std::vector<Record> result;
-    result.emplace_back(current_record_);
-    records_read++;
+    std::vector<Record> records;
 
-    std::string line;
     file_stream.clear();
     file_stream.seekg(current_record_pos_, std::ios::beg);
     if (!file_stream) {
       std::cerr << "seekg failed at position " << current_record_pos_ << '\n';
-      return {result, true}; // Early return to avoid undefined behavior
+      return {records, true}; // Early return to avoid undefined behavior
     }
 
-    while (records_read < record_num && std::getline(file_stream, line)) {
-      Record next_record = Record(line);
-      if (next_record < current_record_) {
-        end_of_run = true;
-        break;
-      }
-      result.emplace_back(Record(line));
-      current_record_ = next_record;
-      records_read++;
+    auto opt_vector = io_manager_->get_memory_page(file_stream);
+    if (!opt_vector) {
+      std::cerr << "Error reading page?\n";
+      return {records, true};
+    }
+    records = std::move(*opt_vector);
+
+    Record last_record = records.back();
+
+    if (last_record < current_record_) {
+      std::cerr << "Memory page is not sorted properly\n";
+      end_of_run = true;
     }
 
-    if (end_of_run) {
-      return {result, end_of_run};
-    }
-
+    current_record_ = last_record;
     if (file_stream.eof()) {
       end_of_run = true;
-      return {result, end_of_run};
+      return {records, end_of_run};
     }
 
-    std::getline(file_stream, line);
-
-    Record next_record = Record(line);
-    if (next_record < current_record_) {
-      end_of_run = true;
-      return {result, end_of_run};
-    }
-
-    current_record_ = next_record;
-    current_record_pos_ = file_stream.tellg();
-
-    return {result, end_of_run};
+    return {records, end_of_run};
   }
 };
