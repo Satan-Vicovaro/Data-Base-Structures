0840b991e711be4501cb9732260e623912936837
Łukasz Kołakowski <kolak.lukass1@gmail.com>
2025-11-09 22:56:50 +0100

    Update to run_test.sh

--- merge_sort_refactor/run_tests.sh
+++ merge_sort_refactor/run_tests.sh
@@ -2,7 +2,28 @@
 
 DATA_FILE="data_file.txt"
 TEST_NUM=20
-FILE_SIZES=(1000 50000 100000 1000000 10000000 50000000 100000000)
+# FILE_SIZES=(1000 50000 100000 1000000 10000000 25000000 50000000 75000000 100000000)
+
+FILE_SIZES=(25000000 50000000 75000000 100000000)
+
+echo "RPP increase"
+for file_size in "${FILE_SIZES[@]}"; do
+  MBC=500
+  MBC_DECREASE_FACTOR=0
+  RPP=500
+  RPP_INCREASE_FACTOR=100
+  for ((i = 0; i < TEST_NUM; i++)); do
+    echo "Record num: ${file_size}" >> "output/${DATA_FILE}"
+    echo -r "t g ${file_size} r q" \
+    | ./output/SortProgram --max_buffer_count ${MBC} --records_per_page ${RPP} \
+    | sed -n '/---Start---/,/---Stop---/p' \
+    >> "output/${DATA_FILE}"
+    ((MBC -= MBC_DECREASE_FACTOR))
+    ((RPP += RPP_INCREASE_FACTOR))
+  done
+done
+
+echo "MBC decrease"
 
 for file_size in "${FILE_SIZES[@]}"; do
   MBC=500
@@ -10,7 +31,7 @@ for file_size in "${FILE_SIZES[@]}"; do
   RPP=500
   RPP_INCREASE_FACTOR=0
   for ((i = 0; i < TEST_NUM; i++)); do
-    echo -r "Record num:${file_size} " >> "output/${DATA_FILE}"
+    echo "Record num: ${file_size}" >> "output/${DATA_FILE}"
     echo -r "t g ${file_size} r q" \
     | ./output/SortProgram --max_buffer_count ${MBC} --records_per_page ${RPP} \
     | sed -n '/---Start---/,/---Stop---/p' \
@@ -20,30 +41,15 @@ for file_size in "${FILE_SIZES[@]}"; do
   done
 done
 
-
-for file_size in "${FILE_SIZES[@]}"; do
-  MBC=500
-  MBC_DECREASE_FACTOR=0
-  RPP=500
-  RPP_INCREASE_FACTOR=100
-  for ((i = 0; i < TEST_NUM; i++)); do
-    echo -r "Record num:${file_size} " >> "output/${DATA_FILE}"
-    echo -r "t g ${file_size} r q" \
-    | ./output/SortProgram --max_buffer_count ${MBC} --records_per_page ${RPP} \
-    | sed -n '/---Start---/,/---Stop---/p' \
-    >> "output/${DATA_FILE}"
-    ((MBC -= MBC_DECREASE_FACTOR))
-    ((RPP += RPP_INCREASE_FACTOR))
-  done
-done
+echo "MBC decrease and  RPP increase"
 
 for file_size in "${FILE_SIZES[@]}"; do
-  MBC=500
-  MBC_DECREASE_FACTOR=30
-  RPP=500
-  RPP_INCREASE_FACTOR=100
+  MBC=1000
+  MBC_DECREASE_FACTOR=40
+  RPP=700
+  RPP_INCREASE_FACTOR=40
   for ((i = 0; i < TEST_NUM; i++)); do
-    echo -r "Record num:${file_size} " >> "output/${DATA_FILE}"
+    echo "Record num: ${file_size}" >> "output/${DATA_FILE}"
     echo -r "t g ${file_size} r q" \
     | ./output/SortProgram --max_buffer_count ${MBC} --records_per_page ${RPP} \
     | sed -n '/---Start---/,/---Stop---/p' \
@@ -52,3 +58,5 @@ for file_size in "${FILE_SIZES[@]}"; do
     ((RPP += RPP_INCREASE_FACTOR))
   done
 done
+
+# echo -r  "t g 12345678 r q" | ./output/SortProgram --max_buffer_count 50 --records_per_page 50 >> output/output.txt
