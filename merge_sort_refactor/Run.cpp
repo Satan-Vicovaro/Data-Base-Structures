ea0f13436ab2dfa0f66d156ec2a00bf9ce37786c
Łukasz Kołakowski <kolak.lukass1@gmail.com>
2025-11-09 15:39:29 +0100

    Added innit function

--- merge_sort_refactor/Run.cpp
+++ merge_sort_refactor/Run.cpp
@@ -3,15 +3,28 @@
 #include <utility>
 
 Run::Run() {
-  current_record_pos_ = 0;
-  current_record_ = Record();
+  current_record_pos = 0;
+  current_record_last = Record();
+  current_record_first = Record();
   end_of_run = false;
 }
 
-Run::Run(std::streampos initial_record_pos_) {
-  current_record_pos_ = initial_record_pos_;
-  current_record_ = Record();
+Run::Run(std::streampos initial_record_pos) {
+  current_record_pos = initial_record_pos;
+  current_record_last = Record();
+  current_record_first = Record();
   end_of_run = false;
+}
+
+void Run::init(Belt &belt) {
+  std::streampos stream_pos = current_record_pos;
+
+  auto [records, end_of_file] = belt.run_read(*this);
+  if (records.size() > 0) {
+    current_record_first = std::move(records.front());
+    current_record_last = std::move(records.back());
+  }
+  current_record_pos = stream_pos;
 }
 
 std::tuple<std::vector<Record>, bool> Run::get_next_records(Belt &belt) {
@@ -20,16 +33,19 @@ std::tuple<std::vector<Record>, bool> Run::get_next_records(Belt &belt) {
 
   std::tie(records, end_of_file) = belt.run_read(*this);
 
-  Record last_record = Record();
+  Record next_last_record = Record();
+  Record next_first_record = Record();
   if (records.size() > 0) {
-    last_record = std::move(records.back());
+    next_first_record = std::move(records.front());
+    next_last_record = std::move(records.back());
   }
 
-  if (last_record < current_record_) {
-    std::cerr << "Memory page is not sorted properly\n";
+  if (current_record_last > next_first_record) {
+    // std::cerr << "Memory page is not sorted properly\n";
     end_of_run = true;
   }
-  current_record_ = last_record;
+  current_record_last = std::move(next_last_record);
+  current_record_first = std::move(next_first_record);
 
   if (end_of_file)
     end_of_run = true;
--- merge_sort_refactor/Run.hpp
+++ merge_sort_refactor/Run.hpp
@@ -9,13 +9,15 @@
 class Belt;
 
 struct Run {
-  std::streampos current_record_pos_;
-  Record current_record_;
+  std::streampos current_record_pos;
+  Record current_record_last;
+  Record current_record_first;
   bool end_of_run;
 
   Run();
 
-  Run(std::streampos initial_record_pos_);
+  Run(std::streampos initial_record_pos);
+  void init(Belt &belt);
 
   std::tuple<std::vector<Record>, bool> get_next_records(Belt &belt);
 };
